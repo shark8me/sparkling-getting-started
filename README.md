02c10fecfd9f138729787c9be0785d66d4c85137
Kiran Karkera <kiran.karkera@gmail.com>
2016-04-11 22:40:37 +0800

    added tutorial for Spark ML support

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Getting started project for [Sparkling](https://gorillalabs.github.io/sparkling/)
 
 This is a companion repo to the sparkling [guide] (https://gorillalabs.github.io/sparkling/articles/tfidf_guide.html). Please see this guide for further information.
+
+## Support for Spark ML
+
+Support for Spark ML has been added in version 1.2.4. Please see this [Gorilla-repl worksheet](http://viewer.gorilla-repl.org/view.html?source=github&user=gorillalabs&repo=sparkling-getting-started>&path=ws/classification-guide.cljw) for a quick tutorial on building ML models.
+
+Alternatively, download [this repo](https://github.com/gorillalabs/sparkling-getting-started), run _lein gorilla_, and open ws/classification-guide.cljw in the Gorilla-repl for an interactive worksheet. 
--- project.clj
+++ project.clj
@@ -4,18 +4,18 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [gorillalabs/sparkling "1.2.4-SNAPSHOT" :exclusions [[javax.servlet/servlet-api]]]]
+                 [gorillalabs/sparkling "1.2.4" :exclusions [[javax.servlet/servlet-api]]]]
 
   :aot [#".*" sparkling.serialization sparkling.destructuring]
   :main tf-idf.core
   :plugins [[lein-gorilla "0.3.6"]]
   :profiles {:provided {:dependencies
-                        [[org.apache.spark/spark-core_2.10 "1.6.0"
+                        [[org.apache.spark/spark-core_2.10 "1.6.1"
                           ;need exclusions thanks to this issue
                           ;https://issues.apache.org/jira/browse/SPARK-1693
                           :exclusions [[javax.servlet/servlet-api]
                                        [org.eclipse.jetty.orbit/javax.servlet]]]
-                         [org.apache.spark/spark-mllib_2.10 "1.6.0"
+                         [org.apache.spark/spark-mllib_2.10 "1.6.1"
                           :exclusions [[javax.servlet/servlet-api]]]
                           ]}
              :dev {:plugins [[lein-dotenv "RELEASE"]]}})
